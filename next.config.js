ae32cc0ac34640c9e7ab649bc24634b199f9fb21
Mariano Dominguez <mariano1491@gmail.com>
2023-06-06 20:55:52 -0300

    add docs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/apidocs',
+                destination: '/docs/index.html'
+            },
+            {
+                source: '/styles/main.css',
+                destination: '/docs/styles/main.css'
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
